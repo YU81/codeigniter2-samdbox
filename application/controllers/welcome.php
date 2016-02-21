236129c7e5b267416336b03919bc27dcccf541f5
YU81 <usui1118@yu81.jp>
2016-02-22 02:52:46 +0900

    Load message board view at welcome controller.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,16 +1,29 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) {
+	exit('No direct script access allowed');
+}
 
-class Welcome extends CI_Controller {
+/**
+ * Class Welcome
+ *
+ * @property CI_Loader          $load
+ * @property CI_Input           $input
+ * @property CI_Session         $session
+ * @property CI_Config          $config
+ * @property CI_Form_validation $form_validation
+ * @property CI_URI             $uri
+ */
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 *        http://example.com/index.php/welcome
+	 *    - or -
+	 *        http://example.com/index.php/welcome/index
+	 *    - or -
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -19,7 +32,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->model('BoardMessage');
+		$bm       = new BoardMessage();
+		$messages = $bm->get_last_entries();
+//		$this->load->view('welcome_message', $messages);
+		$this->load->view('board', $messages);
 	}
 }
 
